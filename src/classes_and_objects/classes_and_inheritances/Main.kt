3c4ac2d58b47f553505bffaecaedc70bfbefaeb3
Kenji Ohtsuka <kok.fdcm@gmail.com>
2018-11-10 13:51:08 +0900

    abstract class

--- src/classes_and_objects/classes_and_inheritances/Main.kt
+++ src/classes_and_objects/classes_and_inheritances/Main.kt
@@ -5,3 +5,22 @@ open class Base1
 // Base class must be initialized after `:`
 class Derived1 : Base1()
 
+abstract class AbstractClass {
+    val int1: Int = 1
+    abstract val int2: Int
+
+    // can not be overridden
+    fun notAbstractMethod() {
+        println("test")
+    }
+
+    abstract fun abstractMethod()
+}
+
+class Derived2: AbstractClass() {
+    override val int2 = 2
+
+    override fun abstractMethod() {
+        println("abstract")
+    }
+}
